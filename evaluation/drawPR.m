719343294e41fddcefe719932922a07790e559db
zhenghaiyong <zhenghaiyong@gmail.com>
2014-11-02 15:10:00 +0800

    Re-evaluate the saliency detection methods by PR curves for removal of PFDN method

--- evaluation/drawPR.m
+++ evaluation/drawPR.m
@@ -2,16 +2,16 @@ function drawPR()
 InputResults = './pr/';
 customColor = cell(10,1);
 
-customColor{1, 1}=[1 1 0];%'y' 'yellow'
-customColor{2, 1}=[1 0 1];%'m' 'magenta'
-customColor{3, 1}=[0 1 1];%'c' 'cyan'
-customColor{4, 1}=[0 1 0];%'g' 'green'
-customColor{5, 1}=[0 0 1];%'b' 'blue'
-customColor{6, 1}=[0 0.7 1];
-customColor{7, 1}=[0.7 0 1];
-customColor{8, 1}=[1 0 0];%'r' 'red'
-customColor{9, 1}=[0.5 0.5 0.5];
-customColor{10, 1}=[0 0 0];%'k' 'black'
+customColor{1, 1}=[1 0 1];%'m' 'magenta'
+customColor{2, 1}=[0 1 1];%'c' 'cyan'
+customColor{3, 1}=[0 1 0];%'g' 'green'
+customColor{4, 1}=[0 0 1];%'b' 'blue'
+customColor{5, 1}=[0.7 0.7 0];
+customColor{6, 1}=[0.7 0 1];
+customColor{7, 1}=[1 0 0];%'r' 'red'
+customColor{8, 1}=[0.5 0.5 0.5];
+customColor{9, 1}=[0 0 0];%'k' 'black'
+customColor{10, 1}=[1 1 0];%'y' 'yellow'
 
 
 traverse(InputResults, customColor);
@@ -37,7 +37,7 @@ for i = 3:length(idsResults)
         series=regexp(InputResults,'/');
         titlename=InputResults((series(end-1)+1):(series(end)-1));
         title(titlename,'FontName','Times');
-        legend_handle=legend('HFT','ITTI','PFDN','PQFT','RC','SIG','SR','IG','S','IG+S');
+        legend_handle=legend('ITTI','SR','PQFT','SIG','HFT','RC','IG','S','IG+S');
         set(legend_handle,'Location','SouthWest','FontName','Times');
         xlabel('Recall','FontName','Times');
         ylabel('Precision','FontName','Times');
