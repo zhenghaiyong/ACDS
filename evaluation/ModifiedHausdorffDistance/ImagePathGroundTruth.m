e9cb01b26f1365336281e10c7836a075ddcd12dd
zhenghaiyong <zhenghaiyong@gmail.com>
2014-12-29 18:33:52 +0800

    Delete useless files

--- evaluation/ModifiedHausdorffDistance/ImagePathGroundTruth.m
+++
@@ -1,6 +0,0 @@
-function [strGroundTruth,idsGroundTruth]=ImagePathGroundTruth()%存放GroundTruth图的路径
-strGroundTruth = './groundtruth/';      
-SuffixGroundTruth = '*.png';  
-PathGroundTruth = strcat(strGroundTruth,SuffixGroundTruth);
-idsGroundTruth = dir(PathGroundTruth);
-
--- evaluation/ModifiedHausdorffDistance/ImagePathMethods.m
+++
@@ -1,7 +0,0 @@
-function [strMethod,idsMethod]=ImagePathMethods(MethodsNum)%存放8中方法图像的路径
-FilePath={'' './method/1Canny/' './method/2ITS/' './method/3Otsu/' './method/4MET/' './method/5Kmeans/' './method/6SalientObjectsSegmentation/' './method/7RemoveCornerNoise/' './method/8WatershedFromMarkers/'};
-%FilePath={'' './1Canny/' './2ITS/' './3Otsu/' './4MET/' './5Kmeans/' './6SalientObjectsSegmentation/' './7RemoveCornerNoise/' './8WatershedFromMarkers/'};
-strMethod = FilePath{MethodsNum};      
-SuffixMethod = '*.tif';  
-PathMethod = strcat(strMethod,SuffixMethod);
-idsMethod = dir(PathMethod);
